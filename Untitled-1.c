2a37da77f625c76c05bcf205e4a68a606a7ace15
Dkbhai999 <dharmilkagada999@gmail.com>
2026-08-13 19:53:40 +0530

    Delete Untitled-1.c

--- Untitled-1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main ()
-{
-    int dol , rup ;
-    printf("enter the amount of dollar ");
-    scanf("%d",dol);
-    rup=dol*90 ;
-    printf("%d dollar\n=rupees\n "dol,rup);
-}

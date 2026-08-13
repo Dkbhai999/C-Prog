1b8e826e5089dda33dce296374a1f28f2b9a0f5d
Dkbhai999 <dharmilkagada999@gmail.com>
2026-08-13 19:53:22 +0530

    convert dollar to rupees

---
+++ dollar2rupees.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main ()
+{
+    int dol , rup ;
+    printf("enter the amount of dollar ");
+    scanf("%d",dol);
+    rup=dol*90 ;
+    printf("%d dollar\n=rupees\n "dol,rup);
+}
